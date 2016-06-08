299c93cbeff886141202890c688cdd924195a23b
Ray <ray@rayps.com>
2016-06-08 17:58:54 +0800

    Create project

---
+++ Octorank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Octorank
+//
+//  Created by ray on 6/8/16.
+//  Copyright © 2016 rayps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
